7de09964413914b2620f4ce03aaca313c24e4915
alkasah <alkasah485@gmail.com>
2023-02-07 17:44:40 -0800

    added more images

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@
 import './App.css';
 import fireflies  from './assets/images/fireflies.webp'
 import whisper from './assets/images/whisper.jpg'
+import ponyo from './assets/images/Ponyo.webp'
+import kiki from './assets/images/kiki.jpg'
+import castle from './assets/images/Castle.webp'
+import kaguya from './assets/images/kaguya.jpg'
+import only from './assets/images/onlyyesterday.webp'
+import totoro from './assets/images/totoro.jpg'
 import Image from './Image'
 
 function App() {
   let data = {
     // img1 : fireflies,
     // img2 : whisper
-    image:[fireflies,whisper]
+    image:[fireflies,whisper,ponyo,kiki,castle,kaguya,only,totoro]
   }
   return (
     <div>
--- src/Image.js
+++ src/Image.js
@@ -5,7 +5,7 @@ export default function Image (props){
     return(
     <div className="gallery">
         {
-            props.mydata.image.map((item,index)=><img src= {item} alt="studioghibli" width="600" height="400"></img>)
+            props.mydata.image.map((item,index)=><img src= {item} alt="studioghibli" width="300" height="200"></img>)
         }
     </div>
     )
